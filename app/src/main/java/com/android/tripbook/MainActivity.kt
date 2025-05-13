699f96efb90426da9972cf7f7201ac49533edef8
ManfoIngrid <manfo.lauraine@ictuniversity.edu.cm>
2025-05-13 13:30:59 +0100

    Change Android to ios

--- app/src/main/java/com/android/tripbook/MainActivity.kt
+++ app/src/main/java/com/android/tripbook/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TripBookTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "ios",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
