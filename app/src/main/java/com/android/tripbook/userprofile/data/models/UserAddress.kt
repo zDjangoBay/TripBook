8caa7f6896730c1cb1d6a74cd95cb1f57ba8e815
Godwill626 <129513968+Godwill626@users.noreply.github.com>
2025-05-22 09:04:52 -0700

    Create UserAddress.kt
    In case the app includes travel-related features or shipping.

---
+++ app/src/main/java/com/android/tripbook/userprofile/data/models/UserAddress.kt
@@ -0,0 +1,9 @@
+package com.android.tripbook.userprofile.data.models
+
+data class UserAddress(
+    val street: String,
+    val city: String,
+    val state: String,
+    val postalCode: String,
+    val country: String
+)
