010a6f991e916abf865e34fea0864cd0fc643781
TheModel <ngohmaximus.ayisih@ictuniversity.edu.cm>
2025-05-23 23:39:58 -0700

    Project Structuring Setup - Model Class
    Here I created the model classes for Company, CompanyContact, CompanyService, CompanyRepository and MockCompanyData which have their role to play as for as this module is concerned.

---
+++ app/src/main/java/com/android/tripbook/companycatalog/model/Company.kt
@@ -0,0 +1,6 @@
+/*
+This class is to model a company with its attributes essential to handle
+company details, user interactions and organizing related information.
+ */
+package com.android.tripbook.companycatalog.model
+
