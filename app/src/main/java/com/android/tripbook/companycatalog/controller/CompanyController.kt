13f3305b6b9ac973b2f548c0c630aabd39fb6c0c
TheModel <ngohmaximus.ayisih@ictuniversity.edu.cm>
2025-05-23 23:23:26 -0700

    Project Structuring Setup - Controller classes
    The controller classes for the Company and the navigation logic have been setup,

---
+++ app/src/main/java/com/android/tripbook/companycatalog/controller/CompanyController.kt
@@ -0,0 +1,6 @@
+/* controller logic
+- To provide an interface for accessing company details through an ID,
+  without managing database interaction.
+*/
+package com.android.tripbook.companycatalog.controller
+
