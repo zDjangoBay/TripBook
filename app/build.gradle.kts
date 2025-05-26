7d1d050bf9f39b0f4d6d92378254e3c86087d980
levelingdev <noumbiap.anthony@ictuniversity.edu.cm>
2025-05-26 01:49:09 +0100

    dependencies added and splash UI for our module designed and dashboard UI also members are to add code and UI and to modify the existing code which the skeleton will be provided but the splash page and dashbaord are my work

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" // Apply KSP plugin here
 }
 
 android {
     namespace = "com.android.tripbook"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.android.tripbook"
         minSdk = 31
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -29,19 +30,26 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         compose = true
+        viewBinding = true
+        dataBinding= true
     }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -51,8 +59,8 @@ android {
 
 dependencies {
     //------------------------------------------------------
-   //           default dependencies on the project
-    //                       do not touch them !!!!!!
+    //           Default dependencies for the project
+    //                  Do not touch them !!!!
     //-----------------------------------------------------
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
@@ -65,6 +73,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -74,6 +84,17 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     //---------------------------------------------------------
-    //      You can add your own dependencies down here
+    //        Custom dependencies
     //---------------------------------------------------------
+
+    implementation("com.github.IsmaelDivita:chip-navigation-bar:1.4.0")
+
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+        ksp("com.github.bumptech.glide:ksp:4.16.0") // Glide's KSP processor
+        // ... other dependencies ...
+
+
+
+
 }
---
+++ app/src/main/java/com/android/tripbook/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.android.tripbook
+
+import android.content.Intent
+import com.android.tripbook.databinding.ActivitySplashBinding
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this@SplashActivity, TripCatalogActivity::class.java))
+        }
+    }
+}
