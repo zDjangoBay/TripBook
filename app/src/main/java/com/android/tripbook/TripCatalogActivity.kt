d77aaeb37abf3eb3e96b1c820f6aed11e5de4d99
levelingdev <noumbiap.anthony@ictuniversity.edu.cm>
2025-05-27 20:30:07 +0100

    recommended trips and upcomming schedules linked to the database for the view of the different trips.Done

--- app/src/main/java/com/android/tripbook/TripCatalogActivity.kt
+++ app/src/main/java/com/android/tripbook/TripCatalogActivity.kt
@@ -5,16 +5,48 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import androidx.recyclerview.widget.LinearLayoutManager
+import android.view.View
+import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.RecyclerView
+import com.android.tripbook.Adapter.RecommendationAdapter
+import com.android.tripbook.Adapter.TripsAdapter
+import com.android.tripbook.ViewModel.MainviewModel
+import com.android.tripbook.databinding.ActivityTripCatalogBinding
 
 class TripCatalogActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTripCatalogBinding
+    private val viewModel: MainviewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_trip_catalog)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding= ActivityTripCatalogBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+       observeViewModel()
+
     }
+
+    private fun observeViewModel() {
+        viewModel.upcomingTrips.observe(this){
+            list->
+            binding.progressBarUpcomming.visibility= View.GONE
+            binding.viewUpcoming.apply {
+                layoutManager=LinearLayoutManager(this@TripCatalogActivity,LinearLayoutManager.HORIZONTAL,false)
+                adapter=TripsAdapter(list)
+            }
+
+        }
+
+        viewModel.recommendedPlaces.observe(this){list->
+            binding.progressBarReconmendated.visibility= View.GONE
+            binding.viewReconmendated.apply {
+                layoutManager=LinearLayoutManager(this@TripCatalogActivity,LinearLayoutManager.HORIZONTAL,false)
+                adapter= RecommendationAdapter(list)
+            }
+    }}
 }
