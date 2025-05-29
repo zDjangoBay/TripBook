71747e69c96327a5bd74a7183991912e7cc9927f
Ngane Emmanuel <emmanuelngane06@gmail.com>
2025-05-29 06:48:28 -0700

    Reapply "Ngane emmanuel tripcatalog"
    This reverts commit 48f0831850bb5d10240c8c766d60b3d7024e3159.

---
+++ app/src/main/java/com/android/tripbook/ui/components/BaseScaffold.kt
@@ -0,0 +1,54 @@
+package com.android.tripbook.ui.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.android.tripbook.ui.theme.Purple40
+
+@Composable
+fun BaseScaffold(
+    navController: NavController,
+    isLoading: Boolean = false,
+    content: @Composable (PaddingValues) -> Unit
+) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route ?: ""
+
+    val title = when {
+        currentRoute.startsWith("home") -> "Home"
+        currentRoute.startsWith("schedule") -> "Schedule Trips"
+        currentRoute.startsWith("catalog") -> "Trip Catalog"
+        currentRoute.startsWith("profile") -> "Profile"
+        currentRoute.startsWith("detail") -> "Trip Details"
+        currentRoute.startsWith("reviews") -> "User Reviews"
+        else -> "TripBook"
+    }
+
+    Scaffold(
+        topBar = { TopBar(title = title) },
+        bottomBar = {
+            BottomNavigationBar(
+                current = currentRoute,
+                onNavigate = {
+                    if (navController.currentDestination?.route != it) {
+                        navController.navigate(it) {
+                            popUpTo(navController.graph.startDestinationId)
+                            launchSingleTop = true
+                        }
+                    }
+                },
+                selectedColor = Purple40
+            )
+        },
+        content = { innerPadding ->
+            Box(modifier = Modifier.fillMaxSize()) {
+                content(innerPadding)
+                ScreenLoader(isLoading)
+            }
+        }
+    )
+}
