d581dec45ca3a4cea696e3f8482a3e790587f3b4
Gnowa_Rickneil <gnowa.gesserickneil@ictuniversity.edu.cm>
2025-05-30 13:55:08 +0100

    Revert "Merge pull request #24 from sas-bergson/trip-scheduling-gnowa-rickneil"
    This reverts commit 5502adad02bc91bc6a49ef5d8c0987eb4215a00e, reversing
    changes made to 47435afebb5a2917d9b1cc265032440abba6b491.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.android.tripbook"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.android.tripbook"
@@ -76,11 +76,4 @@ dependencies {
     //---------------------------------------------------------
     //      You can add your own dependencies down here
     //---------------------------------------------------------
-    val navVersion = "2.9.0"
-
-    implementation("androidx.navigation:navigation-compose:$navVersion")
-    
-    // Date Picker dependencies
-    implementation("androidx.compose.material3:material3:1.3.2")
-    implementation("androidx.compose.material3:material3-window-size-class:1.3.2")
 }
--- app/src/main/java/com/android/tripbook/MainActivity.kt
+++ app/src/main/java/com/android/tripbook/MainActivity.kt
@@ -5,11 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.android.tripbook.data.TripRepository
-import com.android.tripbook.navigation.AppNavigation
+import androidx.compose.ui.tooling.preview.Preview
 import com.android.tripbook.ui.theme.TripBookTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,13 +19,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TripBookTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    AppNavigation()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+fun GreetingPreview() {
+    TripBookTheme {
+        Greeting("Android")
     }
 }
