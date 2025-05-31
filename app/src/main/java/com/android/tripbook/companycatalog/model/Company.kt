6923c1847f3634fd71e90bf0ab73b38fcd25b0dd
wengoh <wengoh.bridaniel@ictuniversity.edu.cm>
2025-05-31 09:04:30 +0100

    I worked on Company.kt, CompanyContact.kt, CompanyService.kt which are model classes for the companycatalog module. it highlights different attributes of a company.

--- app/src/main/java/com/android/tripbook/companycatalog/model/Company.kt
+++ app/src/main/java/com/android/tripbook/companycatalog/model/Company.kt
@@ -3,4 +3,15 @@ This class is to model a company with its attributes essential to handle
 company details, user interactions and organizing related information.
  */
 package com.android.tripbook.companycatalog.model
+data class Company(
+    val id: Int,
+    val name: String,
+    val imageResIds: List<Int>, // List of drawable references
+    val description: String,
+    var likes: Int, // Changed to 'var' so it can be updated
+    var isLiked: Boolean = false, // New: Added mutable isLiked state
+    val stars: Int,
+    val services: List<CompanyService>,
+    val contacts: List<CompanyContact>
+)
 
