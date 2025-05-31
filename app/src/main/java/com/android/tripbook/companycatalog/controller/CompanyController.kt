d48442da5b56f868ed9d918e0f0eb6e2c5c0827e
Joannes18985 <megjohn64@gmail.com>
2025-05-31 13:12:32 +0100

    Worked on the following and controller classes.

--- app/src/main/java/com/android/tripbook/companycatalog/controller/CompanyController.kt
+++ app/src/main/java/com/android/tripbook/companycatalog/controller/CompanyController.kt
@@ -3,4 +3,11 @@
   without managing database interaction.
 */
 package com.android.tripbook.companycatalog.controller
+import com.android.tripbook.companycatalog.model.Company
+import com.android.tripbook.companycatalog.model.CompanyRepository
 
+object CompanyController {
+    fun getCompanyById(id: Int): Company? {
+        return CompanyRepository.getCompanyById(id)
+    }
+}
--- app/src/main/java/com/android/tripbook/companycatalog/controller/NavigationController.kt
+++ app/src/main/java/com/android/tripbook/companycatalog/controller/NavigationController.kt
@@ -2,4 +2,15 @@
 Controller class for app navigation logic
 */
 package com.android.tripbook.companycatalog.controller
+import androidx.navigation.NavHostController
+import com.android.tripbook.companycatalog.navigation.Routes
 
+object NavigationController {
+    fun navigateToDetail(navController: NavHostController, companyId: Int) {
+        navController.navigate("${Routes.DETAIL_SCREEN}/$companyId")
+    }
+
+    fun navigateBack(navController: NavHostController) {
+        navController.popBackStack()
+    }
+}
