38c941f1e1366e8c7e40389432b8a0bad46abc1c
huds-on <seanhudson2004@gmail.com>
2025-06-01 22:15:01 +0100

    itinary builder

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.material.icons.extended)
 
     //---------------------------------------------------------
     //      You can add your own dependencies down here
--- app/src/main/java/com/android/tripbook/MainActivity.kt
+++ app/src/main/java/com/android/tripbook/MainActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.*
+import com.android.tripbook.model.TripViewModel
+import com.android.tripbook.ui.uis.PlanNewTripScreen
+import com.android.tripbook.ui.uis.MyTripsScreen
 import com.android.tripbook.ui.theme.TripBookTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,30 +16,26 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TripBookTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                TripBookApp()
             }
         }
     }
 }
 
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TripBookApp() {
+    var currentScreen by remember { mutableStateOf("MyTrips") }
+    val viewModel = remember { TripViewModel() }
 
-@Preview
-@Composable
-fun GreetingPreview() {
-    TripBookTheme {
-        Greeting("Android")
+    when (currentScreen) {
+        "MyTrips" -> MyTripsScreen(
+            viewModel = viewModel,
+            onPlanNewTripClick = { currentScreen = "PlanNewTrip" },
+            onTripClick = { }
+        )
+        "PlanNewTrip" -> PlanNewTripScreen(
+            viewModel = viewModel,
+            onBackClick = { currentScreen = "MyTrips" }
+        )
     }
 }
