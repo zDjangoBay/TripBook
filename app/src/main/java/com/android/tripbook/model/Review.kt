53af03c5a170bd44f6c65989259fca789dc75f48
Your Name <tchamabe.martinecarole@ictuniversity.edu.cm>
2025-06-01 05:02:03 -0700

    added the class review

--- app/src/main/java/com/android/tripbook/model/Review.kt
+++ app/src/main/java/com/android/tripbook/model/Review.kt
@@ -1,8 +1,10 @@
 package com.android.tripbook.model
 
 data class Review(
+    val id: Int,
     val tripId: Int,
     val username: String,
+    val rating: Int,
     val comment: String,
     val images: List<String>,
     var isLiked: Boolean = false,
