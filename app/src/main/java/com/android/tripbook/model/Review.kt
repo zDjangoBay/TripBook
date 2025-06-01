d68e1557e4134823eb0ec233f37be61739ccabda
Your Name <tchamabe.martinecarole@ictuniversity.edu.cm>
2025-05-31 21:58:02 -0700

    Update review-related files

--- app/src/main/java/com/android/tripbook/model/Review.kt
+++ app/src/main/java/com/android/tripbook/model/Review.kt
@@ -4,5 +4,7 @@ data class Review(
     val tripId: Int,
     val username: String,
     val comment: String,
-    val images: List<String>
+    val images: List<String>,
+    var isLiked: Boolean = false,
+    var isFlagged: Boolean = false
 )
