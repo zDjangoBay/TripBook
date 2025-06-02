50237f0cfe5fc10375011d3f22263264f42c9164
Sukuna52341 <chick.chelseymary@ictuniversity.edu.cm>
2025-06-02 10:36:23 +0100

    err

--- app/src/main/java/com/android/tripbook/TripScheduleRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.android.tripbook
-
-import com.android.tripbook.Schedule
-
-interface TripScheduleRepository {
-    suspend fun getAllSchedules(): List<Schedule>
-    suspend fun getScheduleById(id: String): Schedule?
-}
