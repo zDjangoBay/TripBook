60f4f201cc0add176ea43e769fde3fcea749a352
Fung-Nkoh-Manuel <153408741+Fung-Nkoh-Manuel@users.noreply.github.com>
2025-06-02 20:47:11 +0100

    setup the code to accept catalog

--- app/src/main/java/com/android/tripbook/data/TripItem.kt
+++
@@ -1,9 +0,0 @@
-package com.android.tripbook.data
-
-// Define this in a new file, e.g., 'data/TripItem.kt' or within MainActivity if it's small
-data class TripItem(
-    val id: Int, // A unique ID for each item, useful for keys in LazyColumn
-    val imageUrl: Int, // Or use Int if you're storing drawable resource IDs
-    val cityName: String,
-    val dates: String
-)
