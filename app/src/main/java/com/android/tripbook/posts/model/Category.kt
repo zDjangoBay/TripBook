4ccf46809aee0b5eeacc784ef9078576613177f1
Guegouo M. Guiddel <guegouomoghommahieguiddel@gmail.com>
2025-06-02 19:38:07 +0100

    added files

---
+++ app/src/main/java/com/android/tripbook/posts/model/Category.kt
@@ -0,0 +1,12 @@
+package com.android.tripbook.posts.model
+
+enum class Category(val displayName: String) {
+    ADVENTURE("Adventure"),
+    CULTURE("Culture"),
+    FOOD("Food"),
+    NATURE("Nature"),
+    URBAN("Urban"),
+    BEACH("Beach"),
+    MOUNTAINS("Mountains"),
+    HISTORICAL("Historical")
+}
