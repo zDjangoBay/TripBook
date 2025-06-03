f83cf2a1045809bc1745555485c0615e93bc270f
hugues4 <binwe.hugues@ictuniversity.edu.cm>
2025-06-03 10:50:30 +0100

    define PostRepository interface and add FakePostRepository for testing

--- app/src/main/java/com/android/tripbook/data/repository/PostRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.android.tripbook.data.repository
-
-import com.android.tripbook.model.PostModel
-
-interface PostRepository {
-    suspend fun submitPost(post: PostModel): Result<Unit>
-    suspend fun fetchPosts(): List<PostModel>
-}
--- app/src/main/java/com/android/tripbook/model/PostModel.kt
+++
@@ -1,9 +0,0 @@
-package com.android.tripbook.model
-
-data class PostModel(
-    val title: String = "",
-    val description: String = "",
-    val images: List<String> = emptyList(), // URIs or paths
-    val location: String? = null,
-    val tags: List<String> = emptyList()
-)
