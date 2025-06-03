8535bd93209acfccdc97d5925ce2005cb3b036c7
Ayuknyamndi <bnyamndi@gmail.com>
2025-06-03 16:15:00 +0100

    Modified MainActivity

--- app/src/main/java/com/android/tripbook/MainActivity.kt
+++ app/src/main/java/com/android/tripbook/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.android.tripbook
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,19 +12,50 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.android.tripbook.algorithm.AlgorithmScheduler
 import com.android.tripbook.ui.theme.TripBookTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+    
+    @Inject
+    lateinit var algorithmScheduler: AlgorithmScheduler
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            TripBookTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+        
+        try {
+            // Schedule algorithm workers
+            algorithmScheduler.scheduleAlgorithmWorkers()
+            
+            setContent {
+                TripBookTheme {
+                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        Greeting(
+                            name = "TripBook",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "Error during app initialization", e)
+            // Show a fallback UI instead of crashing
+            setContent {
+                TripBookTheme {
+                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        Text(
+                            text = "Something went wrong. Please try again later.",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
@@ -34,7 +66,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Welcome to $name!",
         modifier = modifier
     )
 }
