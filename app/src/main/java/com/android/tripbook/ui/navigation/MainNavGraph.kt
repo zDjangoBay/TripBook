38b424732c6c9ea8a871480540af5e25e7ef1860
Your Name <tchamabe.martinecarole@ictuniversity.edu.cm>
2025-06-03 07:47:38 -0700

    removed the unused imports

--- app/src/main/java/com/android/tripbook/ui/navigation/MainNavGraph.kt
+++ app/src/main/java/com/android/tripbook/ui/navigation/MainNavGraph.kt
@@ -1,12 +1,8 @@
 package com.android.tripbook.ui.navigation
 import com.android.tripbook.ui.screens.DetailReviewScreen
-
 import AllReviewsScreen
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,7 +10,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
