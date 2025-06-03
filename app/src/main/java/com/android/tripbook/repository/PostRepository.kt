18eae2352434af77415f47bf62b45ce1ca360f6c
niguel07 <bigouawe.clark@ictuniversity.edu.cm>
2025-06-03 14:36:48 +0100

    Define PostRepository interface for data abstraction

--- app/src/main/java/com/android/tripbook/repository/PostRepository.kt
+++ app/src/main/java/com/android/tripbook/repository/PostRepository.kt
@@ -1,2 +1,16 @@
 package com.android.tripbook.repository
 
+import com.android.tripbook.posts.model.PostModel
+import com.android.tripbook.posts.model.Comment
+import kotlinx.coroutines.flow.Flow
+
+interface PostRepository {
+    fun getAllPosts(): Flow<List<PostModel>>
+    fun getPostById(id: String): Flow<PostModel?>
+    suspend fun createPost(post: PostModel)
+    suspend fun updatePost(post: PostModel)
+    suspend fun deletePost(id: String)
+    suspend fun toggleLike(postId: String, userId: String)
+    suspend fun addComment(postId: String, comment: Comment)
+    suspend fun addReply(postId: String, commentId: String, reply: Comment)
+}
