71ee4b08c86ef9eeb91fb0c7671c04a1a4b48fc9
Your Name <tchamabe.martinecarole@ictuniversity.edu.cm>
2025-06-04 04:18:26 -0700

    reorganised the data

--- app/src/main/java/com/android/tripbook/data/SampleReviews.kt
+++ app/src/main/java/com/android/tripbook/data/SampleReviews.kt
@@ -9,7 +9,7 @@ object SampleReviews {
             id = 6,
             tripId = 2,
             rating = 3,
-            username = "Jane Doe",
+            username = "Briyand",
             comment = "Absolutely breathtaking!",
             images = listOf("https://media.gettyimages.com/id/803446314/fr/photo/this-photo-taken-on-june-16-2017-shows-the-city-of-bamenda-the-anglophone-capital-of-northwest.jpg?s=1024x1024&w=gi&k=20&c=x_r8SMF62ult9Xps8g4t8HQJwr6eZvgJH3HOBi8rk48=", "https://media.gettyimages.com/id/141863081/fr/photo/kribi-cameroon-africa.jpg?s=1024x1024&w=gi&k=20&c=Pf5obkANbwDIJfAnkEEGf99sv4ykjtrB9PiYHY2LJ4k=")
         ),
@@ -17,15 +17,15 @@ object SampleReviews {
             id = 9,
             tripId = 2,
             rating = 3,
-            username = "Jane Doe",
+            username = "Carole",
             comment = "Absolutely breathtaking!",
-            images = listOf("https://media.gettyimages.com/id/803446314/fr/photo/this-photo-taken-on-june-16-2017-shows-the-city-of-bamenda-the-anglophone-capital-of-northwest.jpg?s=1024x1024&w=gi&k=20&c=x_r8SMF62ult9Xps8g4t8HQJwr6eZvgJH3HOBi8rk48=", "https://media.gettyimages.com/id/141863081/fr/photo/kribi-cameroon-africa.jpg?s=1024x1024&w=gi&k=20&c=Pf5obkANbwDIJfAnkEEGf99sv4ykjtrB9PiYHY2LJ4k=")
+            images = listOf("https://th.bing.com/th/id/OIP.tyIEwOIscevzwLnKw3XuGwHaE7?w=291&h=194&c=7&r=0&o=5&dpr=1.5&pid=1.7", "https://th.bing.com/th/id/OIP.r1tWienW6NOcnZwPQeV8_AHaE8?w=266&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7")
         ),
         Review(
             id = 8,
             tripId = 2,
             rating = 3,
-            username = "Jane Doe",
+            username = "Emmanuel",
             comment = "Absolutely breathtaking!",
             images = listOf("https://media.gettyimages.com/id/803446314/fr/photo/this-photo-taken-on-june-16-2017-shows-the-city-of-bamenda-the-anglophone-capital-of-northwest.jpg?s=1024x1024&w=gi&k=20&c=x_r8SMF62ult9Xps8g4t8HQJwr6eZvgJH3HOBi8rk48=", "https://media.gettyimages.com/id/141863081/fr/photo/kribi-cameroon-africa.jpg?s=1024x1024&w=gi&k=20&c=Pf5obkANbwDIJfAnkEEGf99sv4ykjtrB9PiYHY2LJ4k=")
         ),
@@ -33,7 +33,7 @@ object SampleReviews {
             id = 5,
             tripId = 2,
             rating = 1,
-            username = "Jane Doe",
+            username = "Marie",
             comment = "Absolutely breathtaking!",
             images = listOf("https://media.gettyimages.com/id/803446314/fr/photo/this-photo-taken-on-june-16-2017-shows-the-city-of-bamenda-the-anglophone-capital-of-northwest.jpg?s=1024x1024&w=gi&k=20&c=x_r8SMF62ult9Xps8g4t8HQJwr6eZvgJH3HOBi8rk48=", "https://media.gettyimages.com/id/141863081/fr/photo/kribi-cameroon-africa.jpg?s=1024x1024&w=gi&k=20&c=Pf5obkANbwDIJfAnkEEGf99sv4ykjtrB9PiYHY2LJ4k=")
         ),
@@ -41,7 +41,7 @@ object SampleReviews {
             id = 3,
             tripId = 1,
             rating = 4,
-            username = "Mark Smith",
+            username = "Brian",
             comment = "Stepping into this historic quarter is like entering a living museum where centuries of human stories continue to unfold on every corner. The architectural diversity is breathtaking Each narrow alley and hidden courtyard reveals new details that speak to the layered history of this remarkable place.\n\nThe guided tours here are exceptional, led by passionate local historians who bring the past to life with engaging storytelling and little-known anecdotes that you won't find in any guidebook. I learned fascinating details about the daily lives of past residents, the evolution of local industries, and how major historical events shaped the character of these neighborhoods.\n\nThe artisan quarter is particularly enchanting, where traditional workshops still operate using time-honored techniques. Watching a master glassblower shape molten glass into delicate sculptures or observing a weaver create intricate textiles on century-old looms provides a profound appreciation for human skill and patience. The local markets offer authentic handcrafted goods that carry the essence of this place's creative spirit, making them meaningful souvenirs that connect you to the cultural heritage long after your visit ends.",
             images = listOf("https://source.unsplash.com/400x300/?temple", "https://source.unsplash.com/400x300/?bali")
         ),
@@ -66,7 +66,7 @@ object SampleReviews {
         Review(
             id = 2,
             tripId = 2,
-            username = "Carole",
+            username = "Anthony",
             rating = 5,
             comment = "Loved every moment!",
             images = listOf(
