723b162f50834911c1ce1fd875fc42fe111e9606
Ebangha <207996483+melo-20@users.noreply.github.com>
2025-06-03 20:29:04 -0700

    Importing necessary classes
    The necessary classes for this particular portion of UI are imported so that they can perform the required job or duty

--- app/src/main/java/com/android/tripbook/companycatalog/ui/components/ImageLoader.kt
+++ app/src/main/java/com/android/tripbook/companycatalog/ui/components/ImageLoader.kt
@@ -3,3 +3,10 @@ This composable provides a reusable, flexible image-loading utility,
 making UI development cleaner and more efficient.
  */
 package com.android.tripbook.companycatalog.ui.components
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
