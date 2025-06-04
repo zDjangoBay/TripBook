19a1509bd4649e441f52f1da57a43caaf684d7e0
Maestro-jr <146443394+Maestro-jr@users.noreply.github.com>
2025-06-04 15:11:51 +0100

    Gradle issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -170,6 +170,7 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.5.4")
     implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
     implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
 
 }
