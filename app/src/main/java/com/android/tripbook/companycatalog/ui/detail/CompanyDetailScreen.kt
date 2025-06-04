995f5d45bd2a129fec0c9840f8678a931a55aad6
Ebangha <130972684+The-Model12@users.noreply.github.com>
2025-06-03 21:01:32 -0700

    Importing Classes
    Importing all the classes necessary for the display of this

--- app/src/main/java/com/android/tripbook/companycatalog/ui/detail/CompanyDetailScreen.kt
+++ app/src/main/java/com/android/tripbook/companycatalog/ui/detail/CompanyDetailScreen.kt
@@ -2,3 +2,40 @@
 This composable presents an interactive and well-organized company profile
  */
 package com.android.tripbook.companycatalog.ui.detail
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.android.tripbook.companycatalog.model.Company
+import androidx.compose.material3.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.draw.clip
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import com.android.tripbook.ui.theme.Purple700
+import com.android.tripbook.companycatalog.model.CompanyRepository
