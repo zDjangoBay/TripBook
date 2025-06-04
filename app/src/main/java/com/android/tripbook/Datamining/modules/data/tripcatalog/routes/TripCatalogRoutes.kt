66c45f1cb3041d8555d4e8bc0d4b23ef112f74dc
MiltonJ23 <jonhmilton1991@gmail.com>
2025-06-04 19:48:56 +0100

    Create models entities and routes for trip catalog
    This commit introduces the following entities:
    - Booking
    - BookingStep
    - Review
    - Trip
    
    It also creates the initial structure for `TripCatalogRoutes`.

---
+++ app/src/main/java/com/android/tripbook/Datamining/modules/data/tripcatalog/routes/TripCatalogRoutes.kt
@@ -0,0 +1,16 @@
+package com.android.Tripbook.Datamining.modules.data.tripcatalog.routes
+
+import io.ktor.server.routing.*
+
+
+fun Route.TripCatalogRoutes(){
+    route("/tripcatalog"){
+
+
+        route("/trips"){
+            post{
+
+            }
+        }
+    }
+}
