2f3fc66eb09a651e2380ce3041361d705cfc1597
MiltonJ23 <jonhmilton1991@gmail.com>
2025-06-04 19:47:12 +0100

    New PostType enum created
    A new enumeration, PostType, has been created.
    It includes the values ORIGINAL, REPLY, and REPOST.

---
+++ app/src/main/java/com/android/tripbook/Datamining/modules/data/posts/model/PostType.kt
@@ -0,0 +1,7 @@
+package com.android.Tripbook.Datamining.modules.data.posts.model
+
+enum class PostType {// Here i want to make a distinction between the original post and a reply post and even a repost
+    ORIGINAL,
+    REPLY,
+    REPOST
+}
