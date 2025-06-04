cef695bb002761d9d42a0ae830b8b495d5c9625d
djouela marie <djouela.rosine@ictuniversity.edu.cm>
2025-06-04 15:17:11 +0100

    Update the Comment Model to Support Replies

--- app/src/main/java/com/android/tripbook/model/Comment.kt
+++ app/src/main/java/com/android/tripbook/model/Comment.kt
@@ -10,5 +10,7 @@ data class Comment(
     val timestamp: String = SimpleDateFormat("MMM dd, yyyy 'at' hh:mm a", Locale.getDefault()).format(Date()),
     val authorName: String = "You",
     val authorAvatar: String? = null,
-    val reactions: MutableMap<String, MutableList<CommentReaction>> = mutableMapOf()
+    val reactions: MutableMap<String, MutableList<CommentReaction>> = mutableMapOf(),
+    val parentId: String? = null, // ID of parent comment if this is a reply
+    val replies: MutableList<Comment> = mutableListOf() // List of replies to this comment
 )
