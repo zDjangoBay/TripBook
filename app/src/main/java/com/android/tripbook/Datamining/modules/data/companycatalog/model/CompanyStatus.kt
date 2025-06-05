6b4519c04dd2aba15dadcb072cb56cc465432848
MiltonJ23 <151161809+MiltonJ23@users.noreply.github.com>
2025-06-05 05:34:38 +0100

    Update of the models of the Company Catalog

--- app/src/main/java/com/android/tripbook/Datamining/modules/data/companycatalog/model/CompanyStatus.kt
+++ app/src/main/java/com/android/tripbook/Datamining/modules/data/companycatalog/model/CompanyStatus.kt
@@ -1,5 +1,8 @@
 package com.android.Tripbook.Datamining.modules.data.companycatalog.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 enum class CompanyStatus { // This will help us define the status of the company whether it was suspended by the MINTRANSPORT or not or even if it was closed
     SUSPENDED,
     RUNNING,
