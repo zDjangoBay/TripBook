ee31f5546ca2578faa61507bff9f97b7445df799
levelingdev <noumbiap.anthony@ictuniversity.edu.cm>
2025-06-05 14:33:02 +0100

    carol's code merged and working

--- app/src/main/java/com/android/tripbook/MainActivity.kt
+++ app/src/main/java/com/android/tripbook/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Immediately launch TripCatalogActivity
+        // Immediately launch HomeActivity
         val intent = Intent(this, HomeActivity::class.java)
         startActivity(intent)
 
