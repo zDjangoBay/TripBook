cd5beee10e820d60f1e26391f043fcf81b497d5e
Ebangha <155795939+Gilgar1@users.noreply.github.com>
2025-06-05 11:51:54 -0700

    Imports of classes

--- app/src/main/java/com/android/tripbook/companycatalog/ui/detail/ContactUs.kt
+++ app/src/main/java/com/android/tripbook/companycatalog/ui/detail/ContactUs.kt
@@ -2,3 +2,26 @@
 This composable streamlines user contact interactions with one-tap actions,
  */
 package com.android.tripbook.companycatalog.ui.detail
+
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext // Import LocalContext
+import androidx.compose.ui.unit.dp
+import com.android.tripbook.companycatalog.model.CompanyContact
+import com.android.tripbook.companycatalog.ui.components.SectionTitle
+import com.android.tripbook.ui.theme.Purple700 // Import Purple700 for heading and button colors
+import androidx.core.net.toUri
+
