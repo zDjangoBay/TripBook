ed10b30dfa1b2112a0849a8e93b8742004c29ba6
yvesfranklin123 <kemadjou.franklin@ictuniversity.edu.cm>
2025-06-06 15:25:57 +0100

    Update Comment.kt

--- app/src/main/java/com/android/tripbook/data/model/Comment.kt
+++ app/src/main/java/com/android/tripbook/data/model/Comment.kt
@@ -1,10 +1,15 @@
 package com.android.tripbook.data.model
 
 
+import java.time.Instant
+
 data class Comment(
     val id: String,
     val postId: String,
     val userId: String,
-    val content: String,
-    val createdAt: Long
+    val username: String,
+    val userAvatar: String?,
+    val text: String,
+    val timestamp: Instant,
+    val replies: List<Comment> = emptyList() // Réponses imbriquées de type Comment canonique
 )
