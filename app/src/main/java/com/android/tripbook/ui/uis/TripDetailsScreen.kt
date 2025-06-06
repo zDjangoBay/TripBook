44c050ac2472f38f097082c074499cba781926fa
huds-on <seanhudson2004@gmail.com>
2025-06-06 20:46:39 +0100

    cleaning

--- app/src/main/java/com/android/tripbook/ui/uis/TripDetailsScreen.kt
+++ app/src/main/java/com/android/tripbook/ui/uis/TripDetailsScreen.kt
@@ -8,13 +8,10 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-//import androidx.compose.ui.draw.clip
-//import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
