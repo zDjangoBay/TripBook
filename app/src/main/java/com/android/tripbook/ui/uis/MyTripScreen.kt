4956c991bb9770cc89ed51950e613bca27f94a23
huds-on <seanhudson2004@gmail.com>
2025-06-06 20:46:34 +0100

    cleaning

--- app/src/main/java/com/android/tripbook/ui/uis/MyTripScreen.kt
+++ app/src/main/java/com/android/tripbook/ui/uis/MyTripScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
@@ -18,20 +17,14 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-//import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.android.tripbook.model.Activity
-
 import com.android.tripbook.model.Trip
 import com.android.tripbook.model.TripStatus
 import com.android.tripbook.model.TripViewModel
-//import java.time.LocalDate
 import java.time.format.DateTimeFormatter
-//import java.util.Locale.filter
 
 @Composable
 fun MyTripsScreen(
