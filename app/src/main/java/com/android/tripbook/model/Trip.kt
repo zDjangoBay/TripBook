b3489d279edfef19ca322dd721660e846cb759e5
Paul Herve <tphtaty@gmail.com>
2025-06-06 16:24:56 +0100

    trip.kt addition

--- app/src/main/java/com/android/tripbook/model/Trip.kt
+++ app/src/main/java/com/android/tripbook/model/Trip.kt
@@ -7,9 +7,8 @@ import java.io.Serializable
  * Represents a travel trip entry in the Trip Catalog.
  * @param id Unique identifier for the trip
  * @param title The name of the trip or destination
- * @param caption A brief summary of the trip
- * @param description A detailed description of the trip
- * @param imageUrl List of images to show for the trip
+ * @param description A brief summary of the trip
+ * @param imageUrl Optional image to show for the trip
  */
 
 data class Trip(
