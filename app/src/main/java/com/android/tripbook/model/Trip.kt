c4dbe5aab099f54945ae752911736c8d97b3281b
huds-on <seanhudson2004@gmail.com>
2025-06-06 20:32:57 +0100

    updating trip data

--- app/src/main/java/com/android/tripbook/model/Trip.kt
+++ app/src/main/java/com/android/tripbook/model/Trip.kt
@@ -1,31 +1,16 @@
 package com.android.tripbook.model
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-//import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
 import java.time.LocalDate
 import java.time.LocalTime
 
-//enum class TripStatus {
-//    PLANNED, ACTIVE, COMPLETED
-//}
-
 data class Trip(
-//    val id: String,
     val name: String,
-    val startDate: LocalDate,
-//    val endDate: LocalDate,
-    val destination: String,
-    val travelers: Int,
-    val budget: Int,
-//    val type: String = "",
-//    val description: String = "",
-//    val activities: List<Activity> = emptyList(),
-//    val expenses: List<Expense> = emptyList(),
-//    val travelersList: List<Traveler> = emptyList()
+    val startDate: String = "",
+    val destination: String = "",
+    val travelers: Int = 1,
+    val budget: Int = 0,
+    val status: TripStatus = TripStatus.PLANNED,
+    val time: String = ""
 )
 
 data class Activity(
@@ -33,17 +18,5 @@ data class Activity(
     val time: LocalTime,
     val title: String,
     val location: String,
-    val status: TripStatus,
     val description: String = ""
-)
-
-data class Expense(
-    val category: String,
-    val description: String,
-    val amount: Int
-)
-
-data class Traveler(
-    val name: String,
-    val isLeader: Boolean = false
 )
