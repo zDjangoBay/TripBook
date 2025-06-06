0b205b157b92ea8752c6003d322731d133e1af84
KING1ADB <kingadb181@gmail.com>
2025-06-06 08:32:33 -0700

    Resolve conflict from merge

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,8 @@ pluginManagement {
                 includeGroupByRegex("com\\.android.*")
                 includeGroupByRegex("com\\.google.*")
                 includeGroupByRegex("androidx.*")
+                includeGroup("com.google.gms")
+
             }
         }
         mavenCentral()
