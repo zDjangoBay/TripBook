305a6536841b39a077e02a68f2ae67957b6af3fe
Browdonche <chetoh.stephan@ictuniversity.edu.cm>
2025-06-06 14:34:33 +0100

    Creating a profile Data Management task P3

--- userprofile/src/main/java/com/tripbook/userprofileBrowdonche/APIservice/Interface/userprofileAPIinterfaceservice.kt
+++ userprofile/src/main/java/com/tripbook/userprofileBrowdonche/APIservice/Interface/userprofileAPIinterfaceservice.kt
@@ -1,5 +1,7 @@
 package com.tripbook.userprofileBrowdonche.APInterface
 
+import com.tripbook.userprofileBrowdonche.Data.UserProfile
+
 interface ApiService {
     suspend fun getUserProfile(userId: String): UserProfile
     // Other API methods...
