6b56fa002c769565697fd3a739f9b176cecb9ba8
AbdouKarimuAkande <153552060+AbdouKarimuAkande@users.noreply.github.com>
2025-06-07 13:20:18 +0200

    Revert "finall"

--- app/src/main/java/com/android/tripbook/companycatalog/model/CompanyContact.kt
+++ app/src/main/java/com/android/tripbook/companycatalog/model/CompanyContact.kt
@@ -1,18 +1,6 @@
-
 /*
 Provide structured way to store and manage various contact details for a company
  */
 
 package com.android.tripbook.companycatalog.model
 
-data class CompanyContact(
-    val phone: String,
-    val email: String,
-    val address: String,
-    val city: String,
-    val region: String,
-    val postalCode: String? = null,
-    val whatsapp: String? = null,
-    val facebook: String? = null,
-    val instagram: String? = null
-)
--- app/src/main/java/com/android/tripbook/companycatalog/model/CompanyService.kt
+++ app/src/main/java/com/android/tripbook/companycatalog/model/CompanyService.kt
@@ -1,4 +1,3 @@
-
 /*
 - This class is used to define and store the different services
   that the company provides.
@@ -6,12 +5,3 @@
 
 package com.android.tripbook.companycatalog.model
 
-data class CompanyService(
-    val id: String,
-    val name: String,
-    val description: String,
-    val price: String,
-    val duration: String? = null,
-    val isPopular: Boolean = false,
-    val availability: String = "Available"
-)
