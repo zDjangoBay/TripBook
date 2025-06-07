113b5e167b545d2898073c6d7a506c741181f469
MiltonJ23 <151161809+MiltonJ23@users.noreply.github.com>
2025-06-07 15:03:39 +0100

    Update BookingStep.kt

--- app/src/main/java/com/android/tripbook/Datamining/modules/data/tripcatalog/models/BookingStep.kt
+++ app/src/main/java/com/android/tripbook/Datamining/modules/data/tripcatalog/models/BookingStep.kt
@@ -1,5 +1,7 @@
 package com.android.Tripbook.Datamining.modules.data.tripcatalog.models
+import kotlinx.serialization.*
 
+@Serializable
 enum class BookingStep {
     DATE_SELECTION,
     TRAVELER_INFO,
