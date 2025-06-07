55abd375ad7c232c64331cb6fcad0b709943ebfb
MiltonJ23 <151161809+MiltonJ23@users.noreply.github.com>
2025-06-07 15:36:22 +0100

    Delete app/src/main/java/com/android/tripbook/Datamining/modules/data/tripcatalog/models/TripOption.kt

--- app/src/main/java/com/android/tripbook/Datamining/modules/data/tripcatalog/models/TripOption.kt
+++
@@ -1,9 +0,0 @@
-package com.android.Tripbook.Datamining.modules.data.tripcatalog.models
-
-data class TripOption(
-                      val id: Int,
-                      val name: String,
-                      val description: String,
-                      val price: Double,
-                      val isSelected: Boolean = false
-                      )
