f4a90bcde655a2cef295833bc7c1b5d8f1889f2b
DevWongibe <wongibe.sahmoah@ictuniversity.edu.cm>
2025-06-07 01:35:01 -1100

    Create Mediia.kt
    Media.kt

---
+++ userprofile/src/main/java/com/tripbook/userprofileWongiberaoul/w/Itemmodels/Mediia.kt
@@ -0,0 +1,25 @@
+package com.tripbook.userprofileWongiberaoul.w.Itemmodels
+
+import java.util.Date
+
+data class MediaItem(
+    val id: String,
+    val name: String,
+    val type: MediaType,
+    val url: String,
+    val createdDate: Date,
+    val modifiedDate: Date,
+    val size: Long
+)
+
+enum class MediaType {
+    PHOTO, VIDEO
+}
+
+enum class MediaFilter {
+    ALL, PHOTOS, VIDEOS
+}
+
+enum class SortOption {
+    CREATION_DATE, MODIFICATION_DATE, NAME
+}
