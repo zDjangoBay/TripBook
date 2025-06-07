23db4505c4b53060c8431a95d5dad100990c0eef
MiltonJ23 <151161809+MiltonJ23@users.noreply.github.com>
2025-06-07 15:09:38 +0100

    Update Review.kt

--- app/src/main/java/com/android/tripbook/Datamining/modules/data/tripcatalog/models/Review.kt
+++ app/src/main/java/com/android/tripbook/Datamining/modules/data/tripcatalog/models/Review.kt
@@ -1,8 +1,12 @@
-package com.android.Tripbook.Datamining.modules.data.tripcatalog.models
+package com.android.tripcatalog.models
 
+import kotlinx.serialization.*
+
+
+@Serializable
 data class Review(
     val tripId: Int,
     val username: String,
     val comment: String,
-    val images: List<String>
+    val images: List<String>?
 )
