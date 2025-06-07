1d9c7257e96a40087a40f4043f5034235c9d4ec8
MiltonJ23 <151161809+MiltonJ23@users.noreply.github.com>
2025-06-07 14:43:54 +0100

    Update UpdatePostRequest.kt

--- app/src/main/java/com/android/tripbook/Datamining/modules/data/posts/model/UpdatePostRequest.kt
+++ app/src/main/java/com/android/tripbook/Datamining/modules/data/posts/model/UpdatePostRequest.kt
@@ -1,11 +1,11 @@
-package com.android.Tripbook.Datamining.modules.data.posts.model
+package com.android.posts.model
 
 import kotlinx.seriaization.*
 
 @Serializable
 data class UpdatePostRequest(
     val value: String?, // Seul le texte du post est généralement modifiable
-    val Post_mediasUrl: List<String>? = null, // Ou la gestion des médias via des routes dédiées
+    val Post_mediasUrl: List<String>? = null, // Ou la gestion des médias via des routes dédiées de MINIO
     val Localisation: String?,
     val Hashtags: List<String>? = null,
     val mention: List<String>? = null
