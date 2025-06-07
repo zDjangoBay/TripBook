fd38f3963cdc3d0a12a617b2c3fa454a055e9279
Ebangha <130972684+The-Model12@users.noreply.github.com>
2025-06-06 22:43:34 -0700

    Create CompanyListItem.kt

---
+++ app/src/main/java/com/android/tripbook/companycatalog/ui/components/CompanyListItem.kt
@@ -0,0 +1,23 @@
+package com.android.tripbook.companycatalog.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.android.tripbook.R
+import com.android.tripbook.companycatalog.data.CompanyData
+import com.android.tripbook.ui.theme.TripbookTheme
+
