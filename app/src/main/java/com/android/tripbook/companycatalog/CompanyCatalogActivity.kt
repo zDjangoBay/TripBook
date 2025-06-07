d8f1b2efd823e9f3c52c07cff0000db594efdf18
Ebangha <130972684+The-Model12@users.noreply.github.com>
2025-06-06 22:49:23 -0700

    Creation of CompanyCatalog Activity

---
+++ app/src/main/java/com/android/tripbook/companycatalog/CompanyCatalogActivity.kt
@@ -0,0 +1,21 @@
+package com.android.tripbook.companycatalog
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.android.tripbook.R
+
+class CompanyCatalogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_company_catalog2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
