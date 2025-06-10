72f932009e453d29e0b512566e1908794bf2c9b4
AbdouKarimuAkande <153552060+AbdouKarimuAkande@users.noreply.github.com>
2025-06-10 15:18:00 +0200

    Delete app/src/main/java/com/android/tripbook/companycatalog directory

--- app/src/main/java/com/android/tripbook/companycatalog/controller/CompanyController.kt
+++
@@ -1,6 +0,0 @@
-/* controller logic
-- To provide an interface for accessing company details through an ID,
-  without managing database interaction.
-*/
-package com.android.tripbook.companycatalog.controller
-
