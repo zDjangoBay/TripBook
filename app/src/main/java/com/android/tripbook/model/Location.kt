9e599edc827fe6cfae722f6a0731706ae1a9f510
Fon Godwill Ivan Tana <ivan8tana@gmail.com>
2025-06-11 09:42:00 +0100

    Refactor location handling: deprecate old Location class, implement new LocationPicker with search functionality, and add LocationPickerViewModel for state management.

--- app/src/main/java/com/android/tripbook/model/Location.kt
+++ app/src/main/java/com/android/tripbook/model/Location.kt
@@ -1,5 +1,7 @@
 package com.android.tripbook.model
 
+// This class is deprecated. Use com.android.tripbook.shared.model.Location instead
+@Deprecated("Use com.android.tripbook.shared.model.Location instead")
 class Location {
 
 }
