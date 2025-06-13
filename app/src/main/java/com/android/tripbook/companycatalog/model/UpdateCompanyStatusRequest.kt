cb245fe6ed2b41dbd53895bfa79f6529e96c6118
Gwagsi-rawlings <129545572+Gwagsi-rawlings@users.noreply.github.com>
2025-06-13 07:36:50 +0100

    Delete app/src/main/java/com/android/tripbook/companycatalog directory

--- app/src/main/java/com/android/tripbook/companycatalog/model/UpdateCompanyStatusRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.android.companycatalog.model
-
-import kotlinx.serialization.Serializable
-
-
-@Serializable
-data class UpdateCompanyStatusRequest(
-    val CompanyStatus: CompanyStatus
-)
