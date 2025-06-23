e0fba66f82ae25400f2aa3dc7a442f0268310025
AssonfackBraynt <assonfack.braynt@ictuniversity.edu.cm>
2025-06-23 06:19:04 +0100

    reajusted image upload

--- app/src/main/java/com/android/tripbook/ui/screens/AddPlaceScreen.kt
+++ app/src/main/java/com/android/tripbook/ui/screens/AddPlaceScreen.kt
@@ -48,7 +48,7 @@ fun AddPlaceScreen(
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.PickMultipleVisualMedia(),
         onResult = { uris ->
-            selectedImages = uris.take(2)
+            selectedImages = uris.take(3)
         }
     )
 
